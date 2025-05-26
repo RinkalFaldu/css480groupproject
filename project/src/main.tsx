366650a73b4b90664ccc33669aaaaaa2bc681f6c
Jovialist <jac@thinkman.com>
2025-05-25 17:15:50 -0700

    Prototype V1.1 Initial

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -1,3 +1,6 @@
+// This is prototype V1.1 for Team 1: Team Leviathan
+// Members: Jac Chambers, Rinkal Faldu, Trey Hansen, Mike Zhang
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
